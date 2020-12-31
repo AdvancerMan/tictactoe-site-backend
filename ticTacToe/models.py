1359be19b8c2941912212ed7a52e314d179e04e1
Kazakov Mihail <advancerman@gmail.com>
2020-12-31 17:00:36 +0700

    Added blank=True for Game.history

--- ticTacToe/models.py
+++ ticTacToe/models.py
@@ -5,7 +5,10 @@ class Game(models.Model):
     width = models.IntegerField()
     height = models.IntegerField()
     winThreshold = models.IntegerField()
-    players = models.JSONField()  # list[int:user_id]
-    colors = models.JSONField()  # list[str:hex_code]
-    history = models.JSONField()  # list[tuple[int:i, int:j, int:user_index]]
+    # list[int:user_id]
+    players = models.JSONField()
+    # list[str:hex_code]
+    colors = models.JSONField()
+    # list[tuple[int:i, int:j, int:user_index]]
+    history = models.JSONField(default=list, blank=True)
     creationTime = models.DateTimeField(auto_now=True)
